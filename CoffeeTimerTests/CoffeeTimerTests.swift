ac576f1e20dbc989b34f2672e1c2007a57daf277
Kutay Demireren <kutay.demireren@pinch.nl>
2023-02-19 22:19:31 +0100

    git git restore .

--- CoffeeTimerTests/CoffeeTimerTests.swift
+++ CoffeeTimerTests/CoffeeTimerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Combine
 @testable import CoffeeTimer
 
 protocol CountdownTimer {
@@ -17,17 +18,20 @@ final class CountdownTimerImpl: CountdownTimer {
 	var isRunning: Bool {
 		return timer != nil
 	}
-
-	private(set) var timeLeft: TimeInterval
-
 	private var timer: Timer?
-	
+
+	@Published private(set) var timeLeft: TimeInterval
+
 	init(timeLeft: TimeInterval) {
 		self.timeLeft = timeLeft
 	}
 
-	func start() {
+	deinit {
+		timer?.invalidate()
+		timer = nil
+	}
 
+	func start() {
 		guard canStart() else {
 			return
 		}
@@ -43,7 +47,15 @@ final class CountdownTimerImpl: CountdownTimer {
 	}
 
 	@objc private func timerDidFire() {
-		timeLeft -= 1
+
+		var newTimeLeft = timeLeft - 1
+
+		if newTimeLeft < 1 {
+			timer?.invalidate()
+			timer = nil
+		}
+
+		self.timeLeft = newTimeLeft
 	}
 }
 
@@ -51,20 +63,22 @@ final class CoffeeTimerTests: XCTestCase {
 
 	var sut: CountdownTimerImpl!
 
+	let defaultInitialTimeLeft = 10.0
+
+	var cancellables: [AnyCancellable] = []
+
     override func setUpWithError() throws {
 
-		sut = CountdownTimerImpl(timeLeft: 3)
+		sut = CountdownTimerImpl(timeLeft: defaultInitialTimeLeft)
     }
 
     func test_start_shouldStartRunning() {
-
 		sut.start()
 
 		XCTAssertTrue(sut.isRunning)
     }
 
 	func test_start_whenTimeLeftIsZero_shouldNotStartRunning() {
-
 		sut = CountdownTimerImpl(timeLeft: 0)
 
 		sut.start()
@@ -73,11 +87,52 @@ final class CoffeeTimerTests: XCTestCase {
 	}
 
 	func test_start_whenTimeLeftIsNegative_shouldNotStartRunning() {
-
 		sut = CountdownTimerImpl(timeLeft: -5)
 
 		sut.start()
 
+		XCTAssertFalse(sut.isRunning)
+	}
+
+	func test_start_shouldCountdownByOne() {
+		let expectedTime = sut.timeLeft - 1
+
+		let exp = expectation(description: "time left should be updated 1 second after start")
+
+		sut.start()
+
+		var resultedTime: Double?
+		let subs = sut.$timeLeft
+			.dropFirst() // skip receiving initial value
+			.sink { timeInterval in
+				resultedTime = timeInterval
+				exp.fulfill()
+			}
+
+		wait(for: [exp], timeout: 1.0)
+
+		subs.cancel()
+		XCTAssertEqual(resultedTime, expectedTime)
+	}
+
+	func test_start_whenTimeLeftReachEnd_shouldStop() {
+		let expectedTime = 3.0
+		sut = CountdownTimerImpl(timeLeft: expectedTime)
+
+		let exp = expectation(description: "time left should reach to 0 in 3 seconds after start")
+
+		sut.start()
+
+		let subs = sut.$timeLeft
+			.sink { timeInterval in
+				if timeInterval == 0 {
+					exp.fulfill()
+				}
+			}
+
+		wait(for: [exp], timeout: expectedTime)
+
+		subs.cancel()
 		XCTAssertFalse(sut.isRunning)
 	}
 }
