7364bf58fa0fe8a51b91e32bb57a3490a42d91c8
Kutay Demireren <kutay.demireren@pinch.nl>
2023-03-13 19:50:41 +0100

    reindentation

--- CoffeeTimer/ContentView.swift
+++ CoffeeTimer/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var body: some View {
+		Circle()
+			.strokeBorder(Color.blue.opacity(0.6), lineWidth: 4)
+			.overlay {
+				Text("00 : 00 : 00")
+					.font(.largeTitle)
+					.foregroundColor(.blue)
+			}
+			.padding(24)
+			.background(Color.black.opacity(0.9))
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
