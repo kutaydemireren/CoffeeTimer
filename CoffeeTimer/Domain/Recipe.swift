6552ec7da0e9a466a5054514b95feb0c9ba969c4
Kutay Demireren <kutay.demireren@pinch.nl>
2023-03-30 21:36:32 +0200

    created base app models

---
+++ CoffeeTimer/Domain/Recipe.swift
@@ -0,0 +1,37 @@
+//
+//  Recipe.swift
+//  CoffeeTimer
+//
+//  Created by Kutay Demireren on 30/03/2023.
+//
+
+import Foundation
+
+struct Recipe {
+	let id: UUID
+	let name: String
+	let ingredients: [Ingredient]
+	let brewQueue: BrewQueue
+}
+
+struct Ingredient {
+	let id: String
+	let name: String
+	let amount: IngredientAmount
+}
+
+struct IngredientAmount {
+	let id: String
+	let type: String
+	let amount: Int
+}
+
+struct BrewQueue {
+	let id: UUID
+	let stages: [BrewStage]
+}
+
+struct BrewStage {
+	let id: UUID
+	let title: String
+}
