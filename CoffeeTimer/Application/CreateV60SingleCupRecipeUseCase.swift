b29a68a3be2032be743d93d903b5d9500bf56979
Kutay Demireren <kutay.demireren@pinch.nl>
2023-04-03 21:58:54 +0200

    extracted CreateV60SingleCupRecipeUseCase into its own file

---
+++ CoffeeTimer/Application/CreateV60SingleCupRecipeUseCase.swift
@@ -0,0 +1,62 @@
+//
+//  CreateV60SingleCupRecipeUseCase.swift
+//  CoffeeTimer
+//
+//  Created by Kutay Demireren on 03/04/2023.
+//
+
+import Foundation
+
+struct CreateV60SingleCupRecipeInputs {
+	let name: String
+	let coffee: IngredientAmount
+	let water: IngredientAmount
+}
+
+protocol CreateV60SingleCupRecipeUseCase {
+	func create(inputs: CreateV60SingleCupRecipeInputs) -> Recipe
+}
+
+struct CreateV60SingleCupRecipeUseCaseImp: CreateV60SingleCupRecipeUseCase {
+
+	private let stageCount: UInt = 5
+
+	func create(inputs: CreateV60SingleCupRecipeInputs) -> Recipe {
+		let stagesBloom = createBloom(inputs: inputs)
+		let stagesBrew: [BrewStage] = createBrew(inputs: inputs)
+		let stageFinish = [BrewStage(action: .finish, requirement: .none)]
+		let stagesAll = stagesBloom + stagesBrew + stageFinish
+
+		return Recipe(
+			name: inputs.name,
+			ingredients: [
+				.init(ingredientType: .coffee, amount: inputs.coffee),
+				.init(ingredientType: .water, amount: inputs.water),
+			],
+			brewQueue: .init(stages: stagesAll)
+		)
+	}
+
+	private func createBloom(inputs: CreateV60SingleCupRecipeInputs) -> [BrewStage] {
+		let waterPerBlock = IngredientAmount(amount: inputs.water.amount / stageCount, type: .gram)
+
+		return [
+			.init(action: .wet, requirement: .none),
+			.init(action: .put(coffee: inputs.coffee), requirement: .none),
+			.init(action: .pour(water: waterPerBlock), requirement: .countdown(5)),
+			.init(action: .pause, requirement: .countdown(5)),
+			.init(action: .swirl, requirement: .countdown(5)),
+			.init(action: .pause, requirement: .countdown(45))
+		]
+	}
+
+	private func createBrew(inputs: CreateV60SingleCupRecipeInputs) -> [BrewStage] {
+		let waterPerBlock = IngredientAmount(amount: inputs.water.amount / stageCount, type: .gram)
+
+		return (1..<stageCount).flatMap { index in
+			let pour = [BrewStage(action: .pour(water: waterPerBlock), requirement: .countdown(index == 1 ? 15 : 10))]
+			let pause = index == stageCount - 1 ? [] : [BrewStage(action: .pause, requirement: .countdown(10))]
+			return pour + pause
+		}
+	}
+}
--- CoffeeTimerTests/Application/CreateV60SingleCupRecipeUseCaseImpTests.swift
+++ CoffeeTimerTests/Application/CreateV60SingleCupRecipeUseCaseImpTests.swift
@@ -8,56 +8,6 @@
 import XCTest
 @testable import CoffeeTimer
 
-struct CreateV60SingleCupRecipeInputs {
-	let name: String
-	let coffee: IngredientAmount
-	let water: IngredientAmount
-}
-
-final class CreateV60SingleCupRecipeUseCaseImp {
-
-	let stageCount: UInt = 5
-
-	func create(inputs: CreateV60SingleCupRecipeInputs) -> Recipe {
-		let stagesBloom = createBloom(inputs: inputs)
-		let stagesBrew: [BrewStage] = createBrew(inputs: inputs)
-		let stageFinish = [BrewStage(action: .finish, requirement: .none)]
-		let stagesAll = stagesBloom + stagesBrew + stageFinish
-
-		return Recipe(
-			name: inputs.name,
-			ingredients: [
-				.init(ingredientType: .coffee, amount: inputs.coffee),
-				.init(ingredientType: .water, amount: inputs.water),
-			],
-			brewQueue: .init(stages: stagesAll)
-		)
-	}
-
-	private func createBloom(inputs: CreateV60SingleCupRecipeInputs) -> [BrewStage] {
-		let waterPerBlock = IngredientAmount(amount: inputs.water.amount / stageCount, type: .gram)
-
-		return [
-			.init(action: .wet, requirement: .none),
-			.init(action: .put(coffee: inputs.coffee), requirement: .none),
-			.init(action: .pour(water: waterPerBlock), requirement: .countdown(5)),
-			.init(action: .pause, requirement: .countdown(5)),
-			.init(action: .swirl, requirement: .countdown(5)),
-			.init(action: .pause, requirement: .countdown(45))
-		]
-	}
-
-	private func createBrew(inputs: CreateV60SingleCupRecipeInputs) -> [BrewStage] {
-		let waterPerBlock = IngredientAmount(amount: inputs.water.amount / stageCount, type: .gram)
-
-		return (1..<stageCount).flatMap { index in
-			let pour = [BrewStage(action: .pour(water: waterPerBlock), requirement: .countdown(index == 1 ? 15 : 10))]
-			let pause = index == stageCount - 1 ? [] : [BrewStage(action: .pause, requirement: .countdown(10))]
-			return pour + pause
-		}
-	}
-}
-
 final class CreateV60SingleCupRecipeUseCaseImpTests: XCTestCase {
 
 	var sut: CreateV60SingleCupRecipeUseCaseImp!
