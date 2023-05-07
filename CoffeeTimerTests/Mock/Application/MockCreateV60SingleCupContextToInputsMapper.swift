c94e374940e73b172fff29cd3b48bfb6ba9abc2d
Kutay Demireren <kutay.demireren@pinch.nl>
2023-05-07 15:10:53 +0200

    fix case CreateV60SingleCupRecipeUseCase create

---
+++ CoffeeTimerTests/Mock/Application/MockCreateV60SingleCupContextToInputsMapper.swift
@@ -0,0 +1,20 @@
+//
+//  MockCreateV60SingleCupContextToInputsMapper.swift
+//  CoffeeTimerTests
+//
+//  Created by Kutay Demireren on 06/05/2023.
+//
+
+import Foundation
+@testable import CoffeeTimer
+
+class MockCreateV60SingleCupContextToInputsMapper: CreateV60SingleCupContextToInputsMapper {
+	var _inputs: CreateV60SingleCupRecipeInputs = .stub
+	var _context: CreateRecipeContext?
+
+	func map(context: CreateRecipeContext) -> CreateV60SingleCupRecipeInputs {
+		self._context = context
+
+		return _inputs
+	}
+}
