c15f50034b84bfef5ad602acfc020dd831b6d77b
Kutay Demireren <kutay.demireren@pinch.nl>
2023-05-21 21:01:00 +0200

    wip - create empty Mapper for RecipeRepository

--- CoffeeTimer/Repository/Recipe/RecipeRepositoryImp.swift
+++ CoffeeTimer/Repository/Recipe/RecipeRepositoryImp.swift
@@ -13,8 +13,27 @@ struct RecipeConstants {
 }
 
 protocol Storage {
-	func save<T>(_ value: T?, forKey key: String)
-	func load<T>(forKey key: String) -> T?
+	func save<T: Codable>(_ value: T?, forKey key: String)
+	func load<T: Codable>(forKey key: String) -> T?
+}
+
+// TODO: Move
+
+protocol RecipeMapperProtocol {
+	func mapToRecipe(recipeDTO: RecipeDTO) -> Recipe
+	func mapToRecipeDTO(recipe: Recipe) -> RecipeDTO
+}
+
+struct RecipeMapper: RecipeMapperProtocol {
+	func mapToRecipe(recipeDTO: RecipeDTO) -> Recipe {
+		// Perform mapping from RecipeDTO to Recipe
+		// Return the mapped Recipe object
+	}
+
+	func mapToRecipeDTO(recipe: Recipe) -> RecipeDTO {
+		// Perform mapping from Recipe to RecipeDTO
+		// Return the mapped RecipeDTO object
+	}
 }
 
 struct StorageImp: Storage {
@@ -39,26 +58,47 @@ final class RecipeRepositoryImp: RecipeRepository {
 	private let savedRecipesKey = RecipeConstants.savedRecipesKey
 
 	private var storage: Storage
+	private var mapper: RecipeMapperProtocol
 
-	init(storage: Storage = StorageImp(userDefaults: .standard)) {
+	init(
+		storage: Storage = StorageImp(userDefaults: .standard),
+		mapper: RecipeMapperProtocol = RecipeMapper()
+	) {
 		self.storage = storage
+		self.mapper = mapper
 	}
+}
 
+extension RecipeRepositoryImp {
 	func getSelectedRecipe() -> Recipe? {
-		return storage.load(forKey: selectedRecipeKey)
-	}
-
-	func getSavedRecipes() -> [Recipe] {
-		return storage.load(forKey: savedRecipesKey) ?? []
-	}
-
-	func save(_ recipe: Recipe) {
-		var newRecipes = getSavedRecipes()
-		newRecipes.append(recipe)
-		storage.save(newRecipes, forKey: savedRecipesKey)
+		if let recipeDTO = storage.load(forKey: selectedRecipeKey) as RecipeDTO? {
+			return mapper.mapToRecipe(recipeDTO: recipeDTO)
+		}
+		return nil
 	}
 
 	func update(selectedRecipe: Recipe) {
-		storage.save(selectedRecipe, forKey: selectedRecipeKey)
+		let recipeDTO = mapper.mapToRecipeDTO(recipe: selectedRecipe)
+		storage.save(recipeDTO, forKey: selectedRecipeKey)
+	}
+}
+
+extension RecipeRepositoryImp {
+	func getSavedRecipes() -> [Recipe] {
+		let recipeDTOs = getSavedRecipeDTOs()
+		return recipeDTOs.map { mapper.mapToRecipe(recipeDTO: $0) }
+	}
+
+	func save(_ recipe: Recipe) {
+		var newRecipeDTOs = getSavedRecipeDTOs()
+		newRecipeDTOs.append(mapper.mapToRecipeDTO(recipe: recipe))
+		storage.save(newRecipeDTOs, forKey: savedRecipesKey)
+	}
+
+	private func getSavedRecipeDTOs() -> [RecipeDTO] {
+		if let recipeDTO = storage.load(forKey: savedRecipesKey) as [RecipeDTO]? {
+			return recipeDTO
+		}
+		return []
 	}
 }
