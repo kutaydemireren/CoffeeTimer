a1dfa656708dff62aa7539d62921416363edd874
Kutay Demireren <kutay.demireren@pinch.nl>
2024-06-01 19:59:27 +0200

    init BrewMethodRepositoryImpTests

---
+++ CoffeeTimerTests/Source/Repository/BrewMethod/BrewMethodRepositoryImpTests.swift
@@ -0,0 +1,24 @@
+//
+//  BrewMethodRepositoryImpTests.swift
+//  CoffeeTimerTests
+//
+//  Created by Kutay Demireren on 01/06/2024.
+//
+
+import XCTest
+@testable import CoffeeTimer
+
+final class BrewMethodRepositoryImpTests: XCTestCase {
+    var sut: BrewMethodRepositoryImp!
+
+    override func setUpWithError() throws {
+        sut = BrewMethodRepositoryImp()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_() throws {
+    }
+}
