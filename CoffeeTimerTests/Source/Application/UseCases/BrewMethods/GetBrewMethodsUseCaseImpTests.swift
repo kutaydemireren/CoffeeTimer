444fa4c3526c44c6d0de6aedc301a01bbe2081f1
Kutay Demireren <kutay.demireren@pinch.nl>
2024-06-01 19:24:43 +0200

    init GetBrewMethodsUseCaseImpTests

---
+++ CoffeeTimerTests/Source/Application/UseCases/BrewMethods/GetBrewMethodsUseCaseImpTests.swift
@@ -0,0 +1,24 @@
+//
+//  GetBrewMethodsUseCaseImpTests.swift
+//  CoffeeTimerTests
+//
+//  Created by Kutay Demireren on 01/06/2024.
+//
+
+import XCTest
+@testable import CoffeeTimer
+
+final class GetBrewMethodsUseCaseImpTests: XCTestCase {
+    var sut: GetBrewMethodsUseCaseImp!
+
+    override func setUpWithError() throws {
+        sut = GetBrewMethodsUseCaseImp()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_() throws {
+    }
+}
