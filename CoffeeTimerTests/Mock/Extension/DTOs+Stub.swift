a4ebf07df8e9caadace8216b199bac5795b51b01
Kutay Demireren <kutay.demireren@pinch.nl>
2024-06-23 19:33:20 +0300

    update DTO stubMini to follow processed info

--- CoffeeTimerTests/Mock/Extension/DTOs+Stub.swift
+++ CoffeeTimerTests/Mock/Extension/DTOs+Stub.swift
@@ -20,9 +20,10 @@ extension BrewMethodDTO {
 extension BrewQueueDTO {
     static var stubMini: BrewQueueDTO {
         BrewQueueDTO(stages: [
-            .init(action: .putCoffee(.init(amount: 10, type: .gram)), requirement: BrewStageRequirementDTO.none, startMethod: .userInteractive, passMethod: .userInteractive, message: "Put all your coffee to brewer"),
-            .init(action: .pourWater(.init(amount: 40, type: .millilitre)), requirement: .countdown(10), startMethod: .userInteractive, passMethod: .auto, message: "To bloom, pour #amount of water\nTotal: #currentTotalWater of water"),
-            .init(action: .pause, requirement: .countdown(30), startMethod: .auto, passMethod: .auto, message: "Let it bloom for #duration")
+            .init(action: .putCoffee(.init(amount: 10, type: .gram)), requirement: BrewStageRequirementDTO.none, startMethod: .userInteractive, passMethod: .userInteractive, message: "Put all your 10.0 grams of coffee to brewer"),
+            .init(action: .pourWater(.init(amount: 40, type: .millilitre)), requirement: .countdown(10), startMethod: .userInteractive, passMethod: .auto, message: "To bloom, pour 40.0 millilitres of water\nTotal: 40.0 millilitres of water"),
+            .init(action: .pourWater(.init(amount: 40, type: .millilitre)), requirement: BrewStageRequirementDTO.none, startMethod: .auto, passMethod: .userInteractive, message: "To bloom, pour 40.0 millilitres of water\nTotal: 80.0 millilitres of water"),
+            .init(action: .pause, requirement: .countdown(30), startMethod: .auto, passMethod: .auto, message: "Let it bloom for 30.0 seconds")
         ])
     }
 }
