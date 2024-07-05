cba9b5f41f18491b1b9b39438b652e0c73c1c189
Kutay Demireren <kutay.demireren@pinch.nl>
2024-07-05 22:56:15 +0200

    update spacing

--- CoffeeTimer/iOS/Views/NumericTextField.swift
+++ CoffeeTimer/iOS/Views/NumericTextField.swift
@@ -9,83 +9,83 @@ import SwiftUI
 
 struct NumericTextField: View {
 
-	enum KeyboardType {
-		case decimal
-		case number
+    enum KeyboardType {
+        case decimal
+        case number
 
-		var uiKeyboardType: UIKeyboardType {
-			switch self {
-			case .decimal:
-				return .decimalPad
-			case .number:
-				return .numberPad
-			}
-		}
-	}
+        var uiKeyboardType: UIKeyboardType {
+            switch self {
+            case .decimal:
+                return .decimalPad
+            case .number:
+                return .numberPad
+            }
+        }
+    }
 
-	let title: String
-	let placeholder: String
-	var keyboardType: KeyboardType = .decimal
-	@FocusState private var isFocused: Bool
+    let title: String
+    let placeholder: String
+    var keyboardType: KeyboardType = .decimal
+    @FocusState private var isFocused: Bool
 
-	@Binding var input: Double
+    @Binding var input: Double
 
-	@State private var displayText: String = ""
+    @State private var displayText: String = ""
 
-	var didUpdate: ((Double) -> Void)?
+    var didUpdate: ((Double) -> Void)?
 
-	var body: some View {
+    var body: some View {
 
-		TitledContent(title: title) {
-			TextField(text: $displayText) {
-				Text(placeholder)
-					.foregroundColor(Color("foregroundPrimary").opacity(0.5))
-			}
-			.textFieldStyle(.plain)
-			.keyboardType(keyboardType.uiKeyboardType)
-			.focused($isFocused)
-			.foregroundColor(Color("foregroundPrimary"))
-			.onChange(of: displayText, perform: filterNonNumerics(_:))
-			.onChange(of: input, perform: setDisplayText(_:))
-			.padding()
-			.backgroundSecondary()
-		}
-	}
+        TitledContent(title: title) {
+            TextField(text: $displayText) {
+                Text(placeholder)
+                    .foregroundColor(Color("foregroundPrimary").opacity(0.5))
+            }
+            .textFieldStyle(.plain)
+            .keyboardType(keyboardType.uiKeyboardType)
+            .focused($isFocused)
+            .foregroundColor(Color("foregroundPrimary"))
+            .onChange(of: displayText, perform: filterNonNumerics(_:))
+            .onChange(of: input, perform: setDisplayText(_:))
+            .padding()
+            .backgroundSecondary()
+        }
+    }
 
-	private func filterNonNumerics(_ newValue: String) {
-		guard isFocused else {
-			return
-		}
+    private func filterNonNumerics(_ newValue: String) {
+        guard isFocused else {
+            return
+        }
 
-		let input = Double(newValue.filteringNonNumerics()) ?? 0.0
-		self.input = input
-		didUpdate?(input)
-	}
+        let input = Double(newValue.filteringNonNumerics()) ?? 0.0
+        self.input = input
+        didUpdate?(input)
+    }
 
-	private func setDisplayText(_ newValue: Double) {
-		if Double(displayText) != newValue {
-			displayText = newValue == 0 ? "" : "\(newValue)"
-		}
-	}
+    private func setDisplayText(_ newValue: Double) {
+        if Double(displayText) != newValue {
+            displayText = newValue == 0 ? "" : "\(newValue)"
+        }
+    }
 }
 
 struct NumericTextField_Previews: PreviewProvider {
-	static var previews: some View {
-		VStack {
-			Spacer()
-			NumericTextField(
-				title: "",
-				placeholder: "placeholder",
-				input: .constant(2.0)) { _ in }
-				.padding()
+    static var previews: some View {
+        VStack {
+            Spacer()
+            NumericTextField(
+                title: "",
+                placeholder: "placeholder",
+                input: .constant(2.0)) { _ in }
+                .padding()
 
-			NumericTextField(
-				title: "How many cups?",
-				placeholder: "placeholder",
-				input: .constant(2.0)) { _ in }
-				.padding()
-			Spacer()
-		}
-		.background(Color.black)
-	}
+            NumericTextField(
+                title: "How many cups?",
+                placeholder: "placeholder",
+                input: .constant(2.0)) { _ in }
+                .padding()
+            Spacer()
+        }
+        .background(Color.black)
+    }
 }
