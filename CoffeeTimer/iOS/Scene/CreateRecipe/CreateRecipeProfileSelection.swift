dd7707897a5df4ac82522eb91b83e35e3f7a4b23
Kutay Demireren <kutay.demireren@pinch.nl>
2024-07-06 15:16:31 +0200

    update spacing

--- CoffeeTimer/iOS/Scene/CreateRecipe/CreateRecipeProfileSelection.swift
+++ CoffeeTimer/iOS/Scene/CreateRecipe/CreateRecipeProfileSelection.swift
@@ -8,174 +8,172 @@
 import SwiftUI
 
 extension RecipeProfile {
-	func updating(name: String) -> RecipeProfile {
-		return RecipeProfile(
-			name: name,
-			icon: icon,
-			cupsCount: cupsCount,
-			ratio: ratio
-		)
-	}
+    func updating(name: String) -> RecipeProfile {
+        return RecipeProfile(
+            name: name,
+            icon: icon,
+            cupsCount: cupsCount,
+            ratio: ratio
+        )
+    }
 
-	func updating(icon: RecipeProfileIcon) -> RecipeProfile {
-		return RecipeProfile(
-			name: name,
-			icon: icon,
-			cupsCount: cupsCount,
-			ratio: ratio
-		)
-	}
+    func updating(icon: RecipeProfileIcon) -> RecipeProfile {
+        return RecipeProfile(
+            name: name,
+            icon: icon,
+            cupsCount: cupsCount,
+            ratio: ratio
+        )
+    }
 }
 
 extension Alignment: CaseIterable {
-	public static var allCases: [Alignment] {
-		return [
-			.top,
-			.bottom,
-			.bottomLeading,
-			.bottomTrailing,
-			.center,
-			.centerFirstTextBaseline,
-			.centerLastTextBaseline,
-			.leadingFirstTextBaseline,
-			.leadingLastTextBaseline,
-			.leading,
-			.topLeading,
-			.topTrailing,
-			.trailing,
-			.trailingFirstTextBaseline,
-			.trailingLastTextBaseline
-		]
-	}
+    public static var allCases: [Alignment] {
+        return [
+            .top,
+            .bottom,
+            .bottomLeading,
+            .bottomTrailing,
+            .center,
+            .centerFirstTextBaseline,
+            .centerLastTextBaseline,
+            .leadingFirstTextBaseline,
+            .leadingLastTextBaseline,
+            .leading,
+            .topLeading,
+            .topTrailing,
+            .trailing,
+            .trailingFirstTextBaseline,
+            .trailingLastTextBaseline
+        ]
+    }
 
-	static var random: Alignment {
-		allCases.randomElement()!
-	}
+    static var random: Alignment {
+        allCases.randomElement()!
+    }
 }
 
 extension CGFloat {
-	static var randomPadding: Self {
-		return CGFloat((16..<24).randomElement() ?? 0)
-	}
+    static var randomPadding: Self {
+        return CGFloat((16..<24).randomElement() ?? 0)
+    }
 
-	static var randomSize: Self {
-		return CGFloat((50..<60).randomElement() ?? 0)
-	}
+    static var randomSize: Self {
+        return CGFloat((50..<60).randomElement() ?? 0)
+    }
 }
 
 class GridCache {
-	let title: String
-	let recipeProfileIcons: [RecipeProfileIcon]
+    let title: String
+    let recipeProfileIcons: [RecipeProfileIcon]
 
-	var widths: [CGFloat] = []
-	var heights: [CGFloat] = []
-	var alignments: [Alignment] = []
-	var offsetX: [CGFloat] = []
-	var offsetY: [CGFloat] = []
-	var rotations: [Angle] = []
+    var widths: [CGFloat] = []
+    var heights: [CGFloat] = []
+    var alignments: [Alignment] = []
+    var offsetX: [CGFloat] = []
+    var offsetY: [CGFloat] = []
+    var rotations: [Angle] = []
 
 
-	init(title: String, recipeProfileIcons: [RecipeProfileIcon]) {
-		self.title = title
-		self.recipeProfileIcons = recipeProfileIcons
+    init(title: String, recipeProfileIcons: [RecipeProfileIcon]) {
+        self.title = title
+        self.recipeProfileIcons = recipeProfileIcons
 
-		setup()
-	}
+        setup()
+    }
 
-	private func setup() {
-		(0..<recipeProfileIcons.count).forEach { index in
-			widths.append(.randomSize)
-			heights.append(.randomSize)
-			alignments.append(.random)
-			offsetX.append(randomOffset())
-			offsetY.append(randomOffset())
-			rotations.append(randomRotation())
-		}
-	}
+    private func setup() {
+        (0..<recipeProfileIcons.count).forEach { index in
+            widths.append(.randomSize)
+            heights.append(.randomSize)
+            alignments.append(.random)
+            offsetX.append(randomOffset())
+            offsetY.append(randomOffset())
+            rotations.append(randomRotation())
+        }
+    }
 
-	private func randomRotation() -> Angle {
-		let degrees = Double.random(in: -30...30)
-		return Angle(degrees: degrees)
-	}
+    private func randomRotation() -> Angle {
+        let degrees = Double.random(in: -30...30)
+        return Angle(degrees: degrees)
+    }
 
-	private func randomOffset() -> CGFloat {
-		let offset = CGFloat.random(in: -8...8)
-		return offset
-	}
+    private func randomOffset() -> CGFloat {
+        let offset = CGFloat.random(in: -8...8)
+        return offset
+    }
 }
 
 struct CreateRecipeProfileSelection: View {
 
-	@Binding var recipeProfile: RecipeProfile
+    @Binding var recipeProfile: RecipeProfile
 
-	let columns: [GridItem] = [
-		GridItem(.flexible(), spacing: 0),
-		GridItem(.flexible(), spacing: 0),
-		GridItem(.flexible(), spacing: 0),
-		GridItem(.flexible(), spacing: 0),
-	]
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+    ]
 
-	var count: Int {
-		gridCache.recipeProfileIcons.count
-	}
+    var count: Int {
+        gridCache.recipeProfileIcons.count
+    }
 
-	let gridCache: GridCache
+    let gridCache: GridCache
 
-	private var nameWrapper: Binding<String> {
-		.init(get: {
-			self.recipeProfile.name
-		}, set: { newValue in
-			self.recipeProfile = recipeProfile.updating(name: newValue)
-		})
-	}
+    private var nameWrapper: Binding<String> {
+        .init(get: {
+            self.recipeProfile.name
+        }, set: { newValue in
+            self.recipeProfile = recipeProfile.updating(name: newValue)
+        })
+    }
 
-	var body: some View {
+    var body: some View {
 
-		VStack {
-
-			VStack {
-				TitledContent(title: gridCache.title) {
-
-					LazyVGrid(columns: columns, spacing: 16) {
-						ForEach(0..<count, id: \.self) { index in
-
-							RecipeProfileIconView(
-								recipeProfileIcon: gridCache.recipeProfileIcons[index],
-								isSelected: recipeProfile.icon == gridCache.recipeProfileIcons[index]
-							)
-							.padding(12)
-							.frame(width: gridCache.widths[index], height: gridCache.heights[index], alignment: gridCache.alignments[index])
-							.rotationEffect(gridCache.rotations[index])
-							.offset(x: gridCache.offsetX[index], y: gridCache.offsetY[index])
-							.onTapGesture {
-								recipeProfile = recipeProfile.updating(icon: gridCache.recipeProfileIcons[index])
-							}
-						}
-					}
-				}
-
-				Separator()
-
-				AlphanumericTextField(title: "Name your recipe", placeholder: "V60 Magic", text: nameWrapper)
-					.multilineTextAlignment(.center)
-					.clearButton(text: nameWrapper)
-			}
-
-			Spacer()
-		}
-		.padding(.horizontal, 32)
-		.contentShape(Rectangle())
-		.onTapGesture {
-			hideKeyboard()
-		}
-	}
+        VStack {
+            VStack {
+                TitledContent(title: gridCache.title) {
+                    
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(0..<count, id: \.self) { index in
+                            
+                            RecipeProfileIconView(
+                                recipeProfileIcon: gridCache.recipeProfileIcons[index],
+                                isSelected: recipeProfile.icon == gridCache.recipeProfileIcons[index]
+                            )
+                            .padding(12)
+                            .frame(width: gridCache.widths[index], height: gridCache.heights[index], alignment: gridCache.alignments[index])
+                            .rotationEffect(gridCache.rotations[index])
+                            .offset(x: gridCache.offsetX[index], y: gridCache.offsetY[index])
+                            .onTapGesture {
+                                recipeProfile = recipeProfile.updating(icon: gridCache.recipeProfileIcons[index])
+                            }
+                        }
+                    }
+                }
+                
+                Separator()
+                
+                AlphanumericTextField(title: "Name your recipe", placeholder: "V60 Magic", text: nameWrapper)
+                    .multilineTextAlignment(.center)
+                    .clearButton(text: nameWrapper)
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 32)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
 }
 
 struct CreateRecipeProfileSelection_Previews: PreviewProvider {
-	static var previews: some View {
-		CreateRecipeProfileSelection(recipeProfile: .constant(.empty), gridCache: gridCache)
-			.backgroundPrimary()
-	}
+    static var previews: some View {
+        CreateRecipeProfileSelection(recipeProfile: .constant(.empty), gridCache: gridCache)
+            .backgroundPrimary()
+    }
 
-	static let gridCache = GridCache(title: TitleStorage.randomFunTitle, recipeProfileIcons: ProfileIconStorage.recipeProfileIcons)
+    static let gridCache = GridCache(title: TitleStorage.randomFunTitle, recipeProfileIcons: ProfileIconStorage.recipeProfileIcons)
 }
--- CoffeeTimer/iOS/Scene/Recipes/RecipeRowView.swift
+++ CoffeeTimer/iOS/Scene/Recipes/RecipeRowView.swift
@@ -8,58 +8,58 @@
 import SwiftUI
 
 struct RecipeProfileIconView: View {
-	let recipeProfileIcon: RecipeProfileIcon
-	var isSelected = false
+    let recipeProfileIcon: RecipeProfileIcon
+    var isSelected = false
 
-	var body: some View {
-		if !recipeProfileIcon.imageName.isEmpty {
-			Image(recipeProfileIcon.imageName)
-				.resizable()
-				.aspectRatio(contentMode: .fit)
-				.foregroundColor(Color("foregroundPrimary"))
-				.opacity(isSelected ? 1.0 : 0.4)
-		}
-	}
+    var body: some View {
+        if !recipeProfileIcon.imageName.isEmpty {
+            Image(recipeProfileIcon.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color("foregroundPrimary"))
+                .opacity(isSelected ? 1.0 : 0.4)
+        }
+    }
 }
 
 struct RecipeProfileView: View {
-	let recipeProfile: RecipeProfile
+    let recipeProfile: RecipeProfile
 
-	var body: some View {
-		HStack {
-			RecipeProfileIconView(recipeProfileIcon: recipeProfile.icon, isSelected: true)
-				.frame(maxHeight: 25)
-			Text(recipeProfile.name)
-				.foregroundColor(Color("foregroundPrimary"))
-				.bold()
-		}
-	}
+    var body: some View {
+        HStack {
+            RecipeProfileIconView(recipeProfileIcon: recipeProfile.icon, isSelected: true)
+                .frame(maxHeight: 25)
+            Text(recipeProfile.name)
+                .foregroundColor(Color("foregroundPrimary"))
+                .bold()
+        }
+    }
 }
 
 struct RecipeRowView: View {
 
-	let recipe: Recipe
+    let recipe: Recipe
 
-	var body: some View {
-		VStack {
-			RecipeProfileView(recipeProfile: recipe.recipeProfile)
-			Text(recipe.ingredients.toRepresentableString)
-				.foregroundColor(Color("foregroundPrimary"))
-				.padding(.leading)
-		}
-		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-		.padding(.horizontal)
-		.backgroundSecondary()
-		.listRowSeparator(.hidden)
-		.listRowBackground(Color.clear)
-	}
+    var body: some View {
+        VStack {
+            RecipeProfileView(recipeProfile: recipe.recipeProfile)
+            Text(recipe.ingredients.toRepresentableString)
+                .foregroundColor(Color("foregroundPrimary"))
+                .padding(.leading)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .padding(.horizontal)
+        .backgroundSecondary()
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+    }
 }
 
 struct RecipeRowView_Previews: PreviewProvider {
-	static var previews: some View {
-		VStack {
-			RecipeRowView(recipe: .stubSingleV60)
-		}
-		.frame(height: 125)
-	}
+    static var previews: some View {
+        VStack {
+            RecipeRowView(recipe: .stubSingleV60)
+        }
+        .frame(height: 125)
+    }
 }
