3c1756bf548d5ca4c3b04f4e4ee2637d4ffea5bc
Kutay Demireren <kutay.demireren@pinch.nl>
2024-07-20 17:56:06 +0200

    extract CreateMethodContext into its own file

--- CoffeeTimer/iOS/Scene/CreateMethod/CreateMethodView.swift
+++ CoffeeTimer/iOS/Scene/CreateMethod/CreateMethodView.swift
@@ -26,70 +26,9 @@ final class CreateMethodContext: ObservableObject {
 }
 
 struct CreateMethodView: View {
-
-    @EnvironmentObject var context: CreateMethodContext
-
     var body: some View {
         VStack {
-            TitledPicker(
-                selectedItem: $context.selectedMethod,
-                allItems: $context.allMethods,
-                title: "Choose a method",
-                placeholder: "Custom"
-            )
-
-            if context.selectedMethod == nil {
-                methodInputs
-            }
-
-            Spacer()
-        }
-        .padding()
-    }
-
-    @ViewBuilder
-    var methodInputs: some View {
-        VStack {
-            AlphanumericTextField(
-                title: "Title will be used to display within app",
-                placeholder: "My V60, Icy V60",
-                text: $context.methodTitle
-            )
-
-            TitledContent(title: "Cups Count Min - Max") {
-                HStack {
-                    VStack(spacing: 0) {
-                        NumericTextField(
-                            title: "",
-                            placeholder: "1",
-                            keyboardType: .number,
-                            range: .init(minimum: 1),
-                            input: $context.cupsCountMin
-                        )
-                        Text("") // this is (and VStack) needed to align with the neighbour content
-                            .font(.footnote)
-                    }
-                    VStack(spacing: 0) {
-                        NumericTextField(
-                            title: "",
-                            placeholder: "5",
-                            keyboardType: .number,
-                            range: .init(minimum: 0),
-                            input: $context.cupsCountMax
-                        )
-                        Text("0 = no limit")
-                            .font(.footnote)
-                    }
-                }
-            }
-
-            TitledContent(title: "") {
-                HStack(alignment: .center) {
-                    Spacer()
-                    Toggle(context.isIcedBrew ? "Iced brew" : "Hot brew", isOn: $context.isIcedBrew)
-                    Spacer()
-                }
-            }
+            CreateMethodDetailsView()
         }
     }
 }
