cc32a9f7a9b9f3574dbeaef8346f2977dd575f5b
Kutay Demireren <kutay.demireren@pinch.nl>
2024-07-20 17:58:05 +0200

    init CreateMethodInstructionsView

---
+++ CoffeeTimer/iOS/Scene/CreateMethod/CreateMethodInstructionsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateMethodInstructionsView.swift
+//  CoffeeTimer
+//
+//  Created by Kutay Demireren on 20/07/2024.
+//
+
+import SwiftUI
+
+struct CreateMethodInstructionsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CreateMethodInstructionsView()
+}
