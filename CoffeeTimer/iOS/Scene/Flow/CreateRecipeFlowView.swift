44aac89c131660ab0597366bbe3e9821a2ca9221
Kutay Demireren <kutay.demireren@pinch.nl>
2024-07-21 18:30:39 +0200

    recover presentation of create recipe (investigating specific changed code)

--- CoffeeTimer/iOS/Scene/Flow/CreateRecipeFlowView.swift
+++ CoffeeTimer/iOS/Scene/Flow/CreateRecipeFlowView.swift
@@ -9,35 +9,14 @@ import SwiftUI
 import Combine
 
 final class CreateRecipeFlowViewModel: ObservableObject, Completable {
-    enum Flow: Hashable {
-        case createMethod
-    }
-
-    @Published var navigationPath: [Flow] = []
     @Published var context = CreateRecipeContext()
 
     var didComplete = PassthroughSubject<CreateRecipeFlowViewModel, Never>()
 
     private var cancellables: [AnyCancellable] = []
 
-    func makeCreateMethodVM() -> CreateMethodFlowViewModel {
-        let viewModel = CreateMethodFlowViewModel()
-        viewModel.didComplete
-            .sink(receiveValue: didComplete)
-            .store(in: &cancellables)
-        return viewModel
-    }
-
     func close() {
         didComplete.send(self)
-    }
-
-    func crateCustomMethod() {
-        navigationPath.append(.createMethod)
-    }
-
-    private func didComplete(_ viewModel: CreateMethodFlowViewModel) {
-        navigationPath.removeAll { $0 == .createMethod }
     }
 }
 
@@ -45,16 +24,7 @@ struct CreateRecipeFlowView: View {
     @StateObject var viewModel: CreateRecipeFlowViewModel
     
     var body: some View {
-        NavigationStack(path: $viewModel.navigationPath) {
-            createRecipe
-        }
-        .navigationDestination(for: CreateRecipeFlowViewModel.Flow.self) { flow in
-            switch flow {
-            case .createMethod:
-                createMethod
-                    .navigationBarBackButtonHidden(true)
-            }
-        }
+        createRecipe
     }
     
     @ViewBuilder
@@ -63,12 +33,6 @@ struct CreateRecipeFlowView: View {
             viewModel: CreateRecipeViewModel(),
             close: viewModel.close
         )
-        .environmentObject(viewModel.context)
-    }
-
-    @ViewBuilder
-    var createMethod: some View {
-        CreateMethodFlowView(viewModel: viewModel.makeCreateMethodVM())
         .environmentObject(viewModel.context)
     }
 }
